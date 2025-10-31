02637472ce3790e30d3ae0902385aa4d9c5cfef1
VAISHALI311 <vaishu50005@gmail.com>
2025-10-31 17:04:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("✅ Production Node.js App Running on Kubernetes!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
 
